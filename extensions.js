c8a8209a91c46aec743aaa369c278a15c3483f87
YRS <yannickromainsegaar@gmail.com>
2024-09-30 14:16:39 +0200

    Change border padding thickness

--- extensions.js
+++ extensions.js
@@ -991,7 +991,7 @@ export const GiftCardDisplayExtension = {
           position: relative;
           max-width: 420px;
           margin: 0 auto;
-          padding: 20px; /* Adjust this value to control border thickness */
+          padding: 10px; /* Adjust this value to control border thickness */
           background-color: #042d62;
           background-image: url('${borderImageUrl}');
           background-size: contain; /* Adjust this to make sure the pattern appears properly */
